5d3b926a064e7787a4587ffa762ec728302c79ce
nahalam <shapla@gmail.com>
2024-05-14 13:01:05 -0700

    multi image with context

--- multi_image.py
+++ multi_image.py
@@ -1,8 +1,16 @@
 import os
 import ollama
 
+
+#model = 'llava-llama3'
+model = 'llava-phi3'
+output_directory = 'output'
+file_name = f"output_ollama_{model}.txt"
+file_path = os.path.join(output_directory, file_name)
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 # Pull the llava-llama3 model
-ollama.pull('llava-llama3')
+ollama.pull(model)
 
 # Directory containing images
 image_dir = './frames'  # Specify the directory containing the images
@@ -11,7 +19,7 @@ image_dir = './frames'  # Specify the directory containing the images
 image_paths = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.jpg') or f.endswith('.png')]
 
 # Initial prompt
-prompt = 'Describe this image:'
+prompt = 'Summarize the image, specially focus on people or vehicle if there is any'
 
 # Accumulator for context
 context = ''
@@ -24,7 +32,7 @@ for image_path in image_paths:
         messages=[
             {
                 'role': 'user',
-                'content': f'Please describe the image given the context {context}',
+                'content': f'Context: {context} Prompt: {prompt}',
                 'images': [image_path]
             }
         ]
@@ -32,13 +40,17 @@ for image_path in image_paths:
 
     # Print the text output
     print("Output:")
-    print(res['message']['content'])
+    output = res['message']['content']
+    print(output)
     print("****************")
+    # Save the output to a text file
+    with open(file_path, 'a') as file:
+        file.write(output+'\n'+'**********'+'\n')
 
     # Update context
-    context += res['message']['content'] + '\n'
+    context += output
 
     # Update prompt for the next iteration
-    prompt = res['message']['content']
-    print("current prompt:", prompt)
-    print("---------------")
+    prompt = context+'\n'+prompt
+    #print("current prompt:", prompt)
+    #print("---------------")
