c301b1c6ae15957ad677eaaa15243a4a20f9095d
nahalam <shapla@gmail.com>
2024-06-03 14:28:05 -0700

    moondream2

--- multi_image_no_context.py
+++ multi_image_no_context.py
@@ -5,9 +5,10 @@ import ollama
 # Start time
 start_time = time.time()
 
-model = 'llava-llama3'
+#model = 'llava-llama3'
 #model = 'llava-phi3'
 #model = 'llava'
+model = 'moondream'
 output_directory = 'output'
 file_name = f"output_ollama_{model}.txt"
 file_path = os.path.join(output_directory, file_name)
@@ -23,7 +24,7 @@ image_dir = './frames'  # Specify the directory containing the images
 image_paths = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.jpg') or f.endswith('.png')]
 
 # Initial prompt
-prompt = 'Summarize the image only if there is any person or vehicle'
+prompt = 'Describe the image'
 
 # Accumulator for context
 #context = ''
